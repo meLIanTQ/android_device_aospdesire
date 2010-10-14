a0d80604458d229d03b1d1bc756091c40c1142e0
meLIanTQ <melian.the.queen@gmail.com>
2010-10-14 18:17:46 +0200

    Update Kernel + FM Support
    Change-Id: Ibcbcfe5f911c739d0acc76b71ce1e2cdf420e1ef

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,8 @@ BOARD_EGL_CFG := device/htc/bravo/egl.cfg
 
 BOARD_USES_QCOM_LIBS := true
 
+BOARD_HAVE_FM_RADIO := true
+
 # # cat /proc/mtd
 # dev:    size   erasesize  name
 # mtd0: 000a0000 00020000 "misc"
